e94a7137584c02f787f2faa103715cc1bf50b2c6
TheGreatS <spiencewelch@gmail.com>
2019-12-09 15:54:24 -0800

    improved the code organization

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const accDenMsg = "You're not cleared for that";
+const helpText = "\n.r to raise your hand\n.l to lower your hand\n.show to show the current queue\n.clear to clear the queue (GM only)";
 var queue = [];
+const ActionEnum = Object.freeze({
+    "help":1,
+    "raise":2,
+    "lower":3,
+    "lowerIt":4,
+    "show":5,
+    "clear":6,
+    "log":7});
 
 client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`);
@@ -26,39 +35,87 @@ client.on('message', msg => {
     const channel = client.channels.get(msg.channel.id);
     const guild = client.guilds.get(msg.member.guild.id);
     const GM = guild.roles.find(val => val.name === "GM");
-    if (msg.content === '.r') {
-        if (!queue.includes(msg.author)) {
-            queue.push(msg.author);
-        }
-        channel.send(queueToString(queue));
-    } else if (msg.content === '.l') {
-        var index = queue.indexOf(msg.author)
-        if (index > -1) {
-            queue.splice(index, 1);
-        }
-        channel.send(queueToString(queue));
-    } else if (msg.content.substring(0, 3) === '.l ') {
-        if (msg.member._roles.indexOf(GM.id) > -1) {
-            var extra = msg.content.substring(3);
-            channel.send("'"+extra+"'");
-        } else {
-            msg.reply(accDenMsg);
-        }
-    } else if (msg.content === '.show') {
-        if (msg.member._roles.indexOf(GM.id) > -1) {
+    let action;
+    // Assign Actions
+    switch(msg.content) {
+        case '.h':
+            action = ActionEnum.help;
+            break;
+        case '.help':
+            action = ActionEnum.help;
+            break;
+        case '.r':
+            action = ActionEnum.raise;
+            break;
+        case '.l':
+            action = ActionEnum.lower;
+            break;
+        case '.show':
+            action = ActionEnum.show;
+            break;
+        case '.clear':
+            action = ActionEnum.clear;
+            break;
+        default:
+            break;
+    }
+    // More Advanced Actions
+    if (msg.content.substring(0, 3) === '.l ') {
+        action = ActionEnum.lowerIt;
+    }
+    if (msg.content.substring(0, 4) === '.log') {
+        action = ActionEnum.log;
+    }
+    // Execute the action
+    switch (action) {
+        case ActionEnum.help:
+            msg.reply(helpText);
+            break;
+        case ActionEnum.raise:
+            if (!queue.includes(msg.author)) {
+                queue.push(msg.author);
+            }
             channel.send(queueToString(queue));
-        } else {
-            channel.send(GM.toString()+" "+queueToString(queue));
-        }
-    } else if (msg.content === '.clear') {
-        if (msg.member._roles.indexOf(GM) > -1) {
-            queue = [];
+            break;
+        case ActionEnum.lower:
+            var index = queue.indexOf(msg.author);
+            if (index > -1) {
+                queue.splice(index, 1);
+            }
             channel.send(queueToString(queue));
-        } else {
-            msg.reply(accDenMsg);
+            break;
+        case ActionEnum.show:
+            if (msg.member._roles.indexOf(GM.id) > -1) {
+                channel.send(queueToString(queue));
+            } else {
+                channel.send(GM.toString()+" "+queueToString(queue));
+            }
+            break;
+        case ActionEnum.clear:
+            if (msg.member._roles.indexOf(GM) > -1) {
+                queue = [];
+                channel.send(queueToString(queue));
+            } else {
+                msg.reply(accDenMsg);
+            }
+            break;
+        case ActionEnum.lowerIt:
+            if (msg.member._roles.indexOf(GM.id) > -1) {
+                var extra = msg.content.substring(3);
+                channel.send("'"+extra+"'");
+            } else {
+                msg.reply(accDenMsg);
+            }
+            break;
+        case ActionEnum.log:
+            console.log(msg);
+            break;
+    }
+    if (msg.isMentioned(client.user)) {
+        var patt = /thanks(,? bot.?|.?)/i
+        if (patt.test(msg.content)) {
+            msg.reply("You're welcome.");
         }
-    } else if (msg.content.substring(0, 4) === '.log') {
-        console.log(msg);
     }
 });
 
